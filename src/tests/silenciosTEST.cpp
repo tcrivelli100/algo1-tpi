c5ecddec4bf1691591a4a0443ef0801c17293db0
panegri <pnegri@gmail.com>
2017-10-30 10:41:29 -0300

    Correccion silenciosTEST

--- src/tests/silenciosTEST.cpp
+++ src/tests/silenciosTEST.cpp
@@ -11,7 +11,7 @@ TEST(silenciosTEST,unAudio){
     int prof = 16;
     int freq = 35;
     audio a = {0,0,0,0,1,2,3,0,0,0,0,0,5,6,0,0,3};
-    lista_intervalos ts = {make_tuple(0, 0.1142),make_tuple(0.2, 0.3428)} ;
+    lista_intervalos ts = {make_tuple(0,0.1143), make_tuple(0.2,0.3429)} ;
 
     lista_intervalos t_out = silencios(a,prof,freq,1);
     cout << "encontrados " << ts.size() << " intervalos" << endl;
@@ -19,7 +19,17 @@ TEST(silenciosTEST,unAudio){
     for(int i=0;i<ts.size();i++){
         intervalo t1 = ts[i];
         intervalo t2 = t_out[i];
-        EXPECT_NEAR(get<0>(t1),get<0>(t2),0.01);
-        EXPECT_NEAR(get<1>(t1),get<1>(t2),0.01);
+        
+        // test inicio del intervalo
+        int m1 = floor(get<0>(t1) * freq);
+        int m2 = floor(get<0>(t2) * freq);
+        
+        EXPECT_EQ(m1,m2);
+        // test fin de intervalo 
+        m1 = floor(get<1>(t1) * freq);
+        m2 = floor(get<1>(t2) * freq);
+        
+        EXPECT_EQ(m1,m2);
+
     }
 }
