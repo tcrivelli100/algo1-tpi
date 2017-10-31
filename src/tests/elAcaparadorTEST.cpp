11ae9da455293f157dc52a58aa13effe0de93449
Pablo Brusco <pablo.brusco@gmail.com>
2017-10-31 13:30:11 -0300

    test pasa las pre

--- src/tests/elAcaparadorTEST.cpp
+++ src/tests/elAcaparadorTEST.cpp
@@ -8,7 +8,7 @@ TEST(elAcaparadorTEST, prueba){
             {10,0,0,9,4,5},
             {5,7,8,9,2,1},
     };
-    int freq = 3;
-    int prof = 4;
+    int freq = 44000;
+    int prof = 16;
     EXPECT_EQ(3, elAcaparador(m, freq, prof));
 }
